82651dd699ee7fea5ddac049317e426ad9d980ff
Sandwich2080 <androidsandwich@163.com>
2019-07-10 18:50:38 +0800

    [Modified] added tabLayout to the homepage.

--- app/src/main/java/com/example/wechatdemo/MainActivity.java
+++ app/src/main/java/com/example/wechatdemo/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.wechatdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setSelectedTabIndicatorColor(Color.WHITE);
     }
 }
