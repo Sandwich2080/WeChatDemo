af445dcf41ca9cbc1fa4f2466c4ade89c511514e
Sandwich2080 <androidsandwich@163.com>
2019-07-22 22:09:49 +0800

    [Modified] modify the titles of tabs.

--- app/src/main/java/com/example/wechatdemo/MainActivity.java
+++ app/src/main/java/com/example/wechatdemo/MainActivity.java
@@ -17,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         tabLayout.setSelectedTabIndicatorColor(Color.WHITE);
+
     }
 }
